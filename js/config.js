bfbeac85b14325a9f4756ec8e77c2c53586d3b0d
ViolettaNespolo2 <56655309+ViolettaNespolo2@users.noreply.github.com>
2026-06-23 14:36:56 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
-  POWER_AUTOMATE_WRITE_URL: 'https://defaultaa83b74bda4449ec89f63ecb67fc50.18.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/8d88f859fdad40c78e7e24f782c6bb00/triggers/manual/paths/invoke?api-version=1',
-  POWER_AUTOMATE_READ_URL:  'https://defaultaa83b74bda4449ec89f63ecb67fc50.18.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/98bcf9db277642e6b8f33c3b55111640/triggers/manual/paths/invoke?api-version=1',
+  POWER_AUTOMATE_WRITE_URL: 'https://defaultaa83b74bda4449ec89f63ecb67fc50.18.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/8d88f859fdad40c78e7e24f782c6bb00/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=CoHDH-6nj3jedRXgv_S2JIS3l8WDzKAHeTWQgg2KzxU',
+  POWER_AUTOMATE_READ_URL:  '',
 };
